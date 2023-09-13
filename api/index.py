7dad900b9f0e2f0b2467336818048074c8c8b407
Mehliug <guilhemferreira6@gmail.com>
2023-09-13 18:20:25 +0200

    dla merde

--- api/index.py
+++ api/index.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-import subprocess
+import subprocess, os
 
 app = Flask(__name__)
-bot_process = subprocess.Popen(['python3' , 'bot.py'])
-
 
 @app.route("/")
 def page_web_de_mort():
-    bot_process
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    bot_process = subprocess.Popen(['python3', os.path.join(current_directory, 'bot.py')])
     return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
-    
+
+if __name__ == "__main__":
+    app.run()
