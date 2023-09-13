277ec4838c30119226d1f29f08d61c23903fb356
Mehliug <guilhemferreira6@gmail.com>
2023-09-13 08:51:16 +0200

    update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
+bot_process = subprocess.Popen(['python3' , '../bot.py'])
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/")
+def page_web_de_mort():
+    #bot_process
+    #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
+    #page_web_de_mort()
+    return "TEST"  
+    
