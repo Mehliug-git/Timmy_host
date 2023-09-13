0537ba471da458c7f2a0b968b14e4eb8823422f2
Mehliug <guilhemferreira6@gmail.com>
2023-09-13 08:52:46 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,8 +7,6 @@ bot_process = subprocess.Popen(['python3' , '../bot.py'])
 
 @app.route("/")
 def page_web_de_mort():
-    #bot_process
-    #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
-    #page_web_de_mort()
-    return "TEST"  
+    bot_process
+    return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
     
