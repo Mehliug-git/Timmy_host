6937528f9e2a80c0f2bb9f26e0fdfaaeb295dffa
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-09-13 13:25:08 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-import subprocess
+import subprocess, os
 
 app = Flask(__name__)
 
 @app.route("/")
 def page_web_de_mort():
-    bot_process = subprocess.Popen(['python3', '/api/bot.py'])
+    bot_process = subprocess.Popen(['python3', os.path.abspath('bot.py')])
     return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
 
 if __name__ == "__main__":
