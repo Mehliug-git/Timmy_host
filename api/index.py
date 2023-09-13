c1b7b80d82b850b6bd91d3f1f4489f8039700ef3
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-09-13 14:49:11 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def page_web_de_mort():
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    bot_process = subprocess.Popen(['pip3', 'install', 'requests','&&','python3', os.path.join(current_directory, 'bot.py')])
+    bot_process = subprocess.Popen(['python3', os.path.join(current_directory, 'bot.py')])
     return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
 
 if __name__ == "__main__":
