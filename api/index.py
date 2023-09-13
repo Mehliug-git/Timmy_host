53e92138a5039c61937ef804ed4da4baa73988c3
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-09-13 13:20:52 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_web_de_mort():
-    bot_process = subprocess.Popen(['python3', 'bot.py'])
+    bot_process = subprocess.Popen(['python3', '/api/bot.py'])
     return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
 
 if __name__ == "__main__":
