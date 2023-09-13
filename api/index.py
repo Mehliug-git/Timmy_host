cc306db6569b6db1396b61a27ffd70623e29cb73
Mehliug <guilhemferreira6@gmail.com>
2023-09-13 08:43:50 +0200

    update

--- api/index.py
+++ api/index.py
@@ -10,15 +10,7 @@ def page_web_de_mort():
     #bot_process
     #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
     #page_web_de_mort()
-    return "TEST"
-
-@app.route("/kill")
-def killswitch():
-    p = subprocess.Popen(f'refresh', stdout=subprocess.PIPE, shell=True)
-    output, error = p.communicate()
-    bot_process
-    return "<html><body><img src='https://i.giphy.com/6jTD6KqVNADI746GOC.gif'></body></html>"
-    #page_web_de_mort()    
+    return "TEST"  
     
 
 app.run()
