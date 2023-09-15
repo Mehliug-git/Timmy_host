8b53bd7e457d9fc401f1082c7dc23e2c94ae5897
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-09-15 16:31:19 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def page_web_de_mort():
     current_directory = os.path.dirname(os.path.abspath(__file__))
     
-    cmd = f"pip3 install requests --user && python3 {os.path.join(current_directory)}/bot.py"
+    cmd = f"python3 {os.path.join(current_directory)}/bot.py"
     os.system(cmd)
     #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'><iframe src='https://timmy-host.vercel.app/api/bot' onload='setInterval(function(){this.src=this.src},4000)'></iframe></body></html>"
     return "test"
