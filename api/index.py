add9e7d2b761aebadd3aa663aaaef6382cfb021c
Mehliug <guilhemferreira6@gmail.com>
2023-09-15 19:26:29 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def page_web_de_mort():
     
-    
+    run_flask()
     #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'><iframe src='https://timmy-host.vercel.app/api/bot' onload='setInterval(function(){this.src=this.src},4000)'></iframe></body></html>"
     return "test"
 
