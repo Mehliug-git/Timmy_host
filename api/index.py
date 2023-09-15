72e63e129e84ff0b3ac553f796fe5753a0bffe50
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-09-15 14:37:48 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_web_de_mort():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    subprocess.Popen(['pip3','install','requests','&&','python3', os.path.join(current_directory, 'bot.py')])
+    cmd = "pip3 install requests && python3 bot.py"
+    os.system(cmd)
     #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'><iframe src='https://timmy-host.vercel.app/api/bot' onload='setInterval(function(){this.src=this.src},4000)'></iframe></body></html>"
     return "test"
 if __name__ == "__main__":
