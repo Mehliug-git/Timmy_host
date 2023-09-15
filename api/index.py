07159904ba0dcf881f76e50852a60518665d76f8
Mehliug <guilhemferreira6@gmail.com>
2023-09-15 18:20:49 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
-import subprocess, os
+import requests, time
 
 app = Flask(__name__)
 
 @app.route("/")
 def page_web_de_mort():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
     
-    cmd = f"python3 {os.path.join(current_directory)}/bot.py"
-    os.system(cmd)
+    
     #return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'><iframe src='https://timmy-host.vercel.app/api/bot' onload='setInterval(function(){this.src=this.src},4000)'></iframe></body></html>"
     return "test"
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
+    
+    while True == True :
+        time.sleep(60)
+        requests.get("https://timmy-host.vercel.app/api/bot")
+
