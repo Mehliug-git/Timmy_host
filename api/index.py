200cd6f2349e5259e9e2d09a1641c500e8047f54
Mehliug <guilhemferreira6@gmail.com>
2023-09-19 22:34:44 +0200

    d

--- api/index.py
+++ api/index.py
@@ -10,43 +10,43 @@ app = Flask(__name__)
 def page_web_de_mort():
 
     #obligé seulement sur vercel snn ça marche po 
-    run_flask_and_quit()
-    le_bot()
-    run_flask()
-    return render_template('index.php'),200
-
-
-def run_flask_and_quit():
-    # process pour run Flask
-    process = multiprocessing.Process(target=app.run)
-
-    # Start le process app.run Flask
-    process.start()
-
-    time.sleep(5)
-
-    #Kill le process Flask
-    process.terminate()
     
-
-def run_flask():
-    # process pour run Flask
-    process = multiprocessing.Process(target=app.run)
-
-    # Start le process app.run Flask
-    process.start()
-
-    time.sleep(5)
-
-    #Kill le process Flask
-    process.terminate()
+    #run_flask()
+    
+    #le_bot() 
+    return render_template('index.php'), run_flask_and_quit
 
 
 def le_bot():
     os.system("python3 bot.py")
 
 
-
-if __name__ == "__main__":
+def run_flask_and_quit():
+    # process pour run Flask
+    print(2)
     
+    try:
+        subprocess.run(['python3', 'bot.py'], timeout=10)
+    except:
+        pass
+
     app.run()
+    
+
+def run_flask():
+    # process pour run Flask
+    process = multiprocessing.Process(target=app.run(port=8089))
+
+    # Start le process app.run Flask
+    #process.start()
+
+    time.sleep(5)
+    print("sleep finit")
+    #Kill le process Flask
+    process.terminate()
+    print("process kill")
+
+
+
+
+run_flask_and_quit()
