14406900e1c2025c0332059acb67a5dce8163d43
Mehliug <guilhemferreira6@gmail.com>
2023-09-20 08:20:26 +0200

    s

--- api/index.py
+++ api/index.py
@@ -1,24 +1,32 @@
 from flask import Flask, render_template
-import subprocess, time
+import subprocess, time, keyboard
 import multiprocessing, os
 
 app = Flask(__name__)
 
+def simulate_ctrl_c():
+    time.sleep(2)
+    keyboard.press_and_release('ctrl+c')
+    run_flask_and_quit()
+    return "sqd"
 
 
 @app.route("/")
 def page_web_de_mort():
-
+    simulate_ctrl_c()  # Appeler la fonction pour simuler Ctrl+C
     return render_template('index.php')
 
+
+
+
 def run_flask_and_quit():
-    
+    # process pour run Flask
+    print(2)
     try:
-        subprocess.run(['python3', 'bot.py'], timeout=5)
+        subprocess.run(['python3', 'bot.py'], timeout=10)
     except:
         pass
 
-    app.run()
     
+app.run()   
 
-run_flask_and_quit()
