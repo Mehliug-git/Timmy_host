84e2b57389e199bf266fcb352af4f6b4e3d143ae
Mehliug <guilhemferreira6@gmail.com>
2023-09-20 18:04:20 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,33 +1,24 @@
+import subprocess
+import threading
 from flask import Flask, render_template
-import subprocess, time, keyboard
-import multiprocessing, os
 
 app = Flask(__name__)
 
-
-#ctrlc = keyboard.press_and_release('ctrl+c')
-
-def simulate_ctrl_c():
-    time.sleep(2)
-    keyboard.press_and_release('ctrl+c')
-    run_flask_and_quit()
-    return "sqd"
-
-
 @app.route("/")
 def page_web_de_mort():
-    run_flask_and_quit()  # Appeler la fonction pour simuler Ctrl+C
+    # thread exec run_bot() en bg
+    bot_thread = threading.Thread(target=run_bot)
+    bot_thread.start()
+
     return render_template('index.php')
 
-
-def run_flask_and_quit():
-    # process pour run Flask
-    print(2)
+def run_bot():
+    # process pour exécuter le bot
+    print("bot start heheheha")
     try:
-        subprocess.run(['nohup','python3', '../bot.py'], timeout=5)
+        subprocess.run(['python3', 'bot.py'])
     except:
         pass
 
-    
-app.run()   
-
+if __name__ == "__main__":
+    app.run()
