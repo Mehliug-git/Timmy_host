d3807ecf7a98390d937ec0fd3a064d8718801823
Guilhem Ferreira <guilhemferreira6@gmail.com>
2023-09-21 12:02:28 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,18 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def page_web_de_mort():
     # thread exec run_bot() en bg
-    """
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.start()
-    """
-    subprocess.run(run_bot)
+
     return render_template('index.php')
 
 def run_bot():
     # process pour exécuter le bot
     print("bot start heheheha")
-    
-    os.system("python3 bot.py")
+    path = os.getcwd()
+    os.system(f"python3 {path}/bot.py")
 
 
 if __name__ == "__main__":
