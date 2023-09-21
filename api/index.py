0a18e69399d6102828283360bcf17af5f66e2336
Mehliug <guilhemferreira6@gmail.com>
2023-09-21 08:31:13 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
-import threading
+import threading, subprocess
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,18 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def page_web_de_mort():
     # thread exec run_bot() en bg
+    """
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.start()
-
+    """
+    subprocess.run(run_bot)
     return render_template('index.php')
 
 def run_bot():
     # process pour exécuter le bot
     print("bot start heheheha")
     
-    t = os.system("python3 bot.py")
-    test = threading.Thread(target=t)
-    test.start()
+    os.system("python3 bot.py")
 
 
 if __name__ == "__main__":
